067e4543104bfcee0306a0adeebddf4bff40cc37
Joel Grant <joelmgrant@me.com>
2022-11-08 20:43:38 -0700

    initial commit

---
+++ src/main/java/com/example/springbootplaygroundofjoy/SpringBootPlaygroundOfJoyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootplaygroundofjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlaygroundOfJoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPlaygroundOfJoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootplaygroundofjoy/SpringBootPlaygroundOfJoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootplaygroundofjoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlaygroundOfJoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
